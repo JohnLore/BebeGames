cd195b13a6693a4d9e70f605983c647f2fec04ba
unknown <cheather@andrew.cmu.edu>
2015-11-08 03:03:21 -0500

    New camera model with collisions

---
+++ TBDdev1.0/Assets/Scripts/CameraScripts/SphereFree.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A camera script which assumes no rotations or scaling of the player it is attatched to
+public class SphereFree : MonoBehaviour {
+	
+	public GameObject player;
+	public float xSensitivity;
+	public float ySensitivity;
+	public float zoomSensitivity;
+
+	//Store data as a unit vector along with a magnitude
+	private Vector3 direction;
+	private float magnitude;
+	
+	void Start ()
+	{
+		Vector3 initial = transform.position - player.transform.position;
+		magnitude = Vector3.Magnitude (initial);
+		direction = Vector3.Normalize(initial);
+	}
+
+	void Update()
+	{
+		//Treat a horizontal movement as a rotation about y
+		//Treat a vertical movement as a rotation in the plane
+		//parallel to y, containing the cameras position.
+		float moveHorizontal = Input.GetAxis ("HorizontalR");
+
+		//inverted/uninverted y-axis control set by Menu -- default inverted
+		float moveVertical;
+		if (GlobalVars.invertYAxis) //invertYAxis is global
+			moveVertical = 1.0f * Input.GetAxis ("VerticalR");
+		else
+			moveVertical =  -1.0f * Input.GetAxis ("VerticalR");
+
+		float Theta = moveVertical * ySensitivity;
+		float Phi = moveHorizontal* xSensitivity;
+		//Calculate trigs
+		float sinTheta = Mathf.Sin (Theta);
+		float cosTheta = Mathf.Cos (Theta);
+
+		Vector4 row1 = new Vector4 (cosTheta, -sinTheta, 0.0f, 0.0f);
+		Vector4 row2 = new Vector4 (sinTheta,  cosTheta, 0.0f, 0.0f);
+
+		float sinPhi = Mathf.Sin (Phi);
+		float cosPhi = Mathf.Cos (Phi);
+
+		Vector4 row1Y = new Vector4 (cosPhi, 0.0f, -sinPhi, 0.0f);
+		Vector4 row3Y = new Vector4 (sinPhi, 0.0f,  cosPhi, 0.0f);
+
+
+		//First get the components x=a,y=b,z=c from the direction vector3
+		float a = direction.x;
+		float b = direction.y;
+		float c = direction.z;
+
+
+		//Construct basis uyvw from xyzw
+
+		//[a,0,b,0]
+		Vector4 u = Vector3.Normalize(new Vector4 (a, 0.0f, c, 0.0f));
+		//[0,1,0,0]
+		Vector4 y = new Vector4 (0.0f, 1.0f, 0.0f, 0.0f);
+		//[c,0,-a,0]
+		Vector4 v = Vector3.Normalize (new Vector4 (c, 0.0f, -a, 0.0f));
+
+		Vector4 w = new Vector4 (0.0f, 0.0f, 0.0f, 1.0f);
+
+		Vector4 zer0 = Vector4.zero;
+
+		float aPrime = Vector3.Dot (u, Vector3.right);
+		float cPrime = Vector3.Dot (u, Vector3.forward);
+
+		float det = aPrime * aPrime + cPrime * cPrime;
+		//Calculate Matrix Transformations
+		Matrix4x4 toBasis = Matrix4x4.zero;
+		toBasis.SetRow (0, u);
+		toBasis.SetRow (1, y);
+		toBasis.SetRow (2, v);
+		toBasis.SetRow (3, w);
+
+		Matrix4x4 rotateV = Matrix4x4.zero;
+		rotateV.SetRow (0,row1);
+		rotateV.SetRow (1, row2);
+		rotateV.SetRow (2, new Vector4(0.0f,0.0f,1.0f,0.0f));
+		rotateV.SetRow (3, w);
+
+		Matrix4x4 rotateY = Matrix4x4.zero;
+		rotateY.SetRow (0, row1Y);
+		rotateY.SetRow (1, y);
+		rotateY.SetRow (2, row3Y);
+		rotateY.SetRow (3, w);
+
+		Matrix4x4 frBasis = Matrix4x4.zero;
+		frBasis.SetRow (0, u / det);
+		frBasis.SetRow (1, y);
+		frBasis.SetRow (2, v / det);
+		frBasis.SetRow (3, w);
+
+
+		//Compact matrix transformations
+		Matrix4x4 rotate;
+		rotate = toBasis * rotateY * rotateV * frBasis;
+
+		direction = rotate * direction;
+
+
+
+		//TODO: fix -- when reset game from menu, the light gets weird
+
+		//TODO: fix -- when player looks straight up (holds down up) its spazzes
+
+		//Zoom/fade
+		float deltaZoom = Input.GetAxis ("Mouse ScrollWheel");
+		magnitude += deltaZoom * zoomSensitivity;
+
+	}
+	void LateUpdate ()
+	{
+		transform.position = magnitude * direction + player.transform.position;
+		transform.LookAt (player.transform.position);
+	}
+}
---
+++ TBDdev1.0/Assets/Scripts/CameraScripts/WindWakerRay.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class WindWakerRay : MonoBehaviour {
+	public GameObject target;
+	
+
+	private Collider targetCollider;
+
+	public static float turnSensitivity = 0.08f;
+	public static float liftSensitivity = 0.3f;
+		
+	private static float minViewDistance = 3.0f;
+	private static float maxViewDistance = 25.0f;
+	private static float minHeight = 0.0f;
+	private static float maxHeight = 15.0f;
+	private float presumedRadius;
+	private Vector2 wallDirection;
+
+	private float turnDiminish = 10.0f;
+	private float turn;
+	private float lift;
+
+	private static float a = 9.0f;
+	private static float deltaHeight = maxHeight - minHeight;
+	private static float deltaView = (maxViewDistance-minViewDistance);
+	private static float b = (maxHeight-minHeight)/(Mathf.Sqrt(1+(deltaView*deltaView)/(a*a))-1);
+
+
+
+	/*
+	 * 1 = (y/b)^2 -(x/a)^2 
+	 */
+	private float height(float radius)
+	{
+
+		float r = radius - minViewDistance;
+		float y = b*(Mathf.Sqrt (1+(r*r)/(a*a)) - 1);
+
+		return y;
+
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		targetCollider = GetComponentInParent<Collider> ();
+		presumedRadius = Mathf.Sqrt (transform.position.x*transform.position.x+transform.position.z*transform.position.z);
+	}
+
+	private Vector3 detectWall()
+	{
+		Vector3 start = target.transform.position;
+		Vector3 direction = transform.position - target.transform.position;
+		Ray ray = new Ray(start, direction);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, presumedRadius))
+		{
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+
+	private void rotateSide(float angle)
+	{
+		float cosTurn = Mathf.Cos (angle);
+		float sinTurn = Mathf.Sin (angle);
+		
+		float xPos = transform.position.x - target.transform.position.x;
+
+		float zPos = transform.position.z - target.transform.position.z;
+
+		float xPred = xPos * cosTurn - zPos * sinTurn + target.transform.position.x;
+		float zPred = xPos * sinTurn + zPos * cosTurn + target.transform.position.z;
+		Vector3 newPosition = new Vector3 (xPred, transform.position.y, zPred);
+		transform.position = newPosition;
+	}
+
+	private void zoom(float intensity)
+	{
+		//Reset to presumedRadius
+		float curx = transform.position.x - target.transform.position.x;
+		float curz = transform.position.z - target.transform.position.z;
+
+		float currentRadius = Mathf.Sqrt (curx*curx + curz*curz);
+		float scaleFactor = presumedRadius / currentRadius;
+		float xPred = curx * scaleFactor;
+		float zPred = curz * scaleFactor;
+
+
+
+		//Attempt zoom
+		if ((presumedRadius > maxViewDistance && intensity > 0) ||
+			(presumedRadius < minViewDistance && intensity < 0)) 
+		{
+			intensity = 0.0f; 
+		}
+	
+		Vector3 newPosition = new Vector3 (xPred, 0.0f, zPred);
+		newPosition += intensity * Vector3.Normalize (newPosition); 
+
+		presumedRadius = Vector3.Magnitude (newPosition);
+
+		newPosition += target.transform.position;
+
+		transform.position = newPosition;
+	}
+
+	private void adjustHeight()
+	{
+		float xPred = transform.position.x;
+		float yPred = minHeight + height(presumedRadius)+target.transform.position.y;
+		float zPred = transform.position.z;
+		Vector3 newPosition = new Vector3 (xPred, yPred, zPred);
+		transform.position = newPosition;
+	}
+
+	public void getInputs()
+	{
+		turn = turnSensitivity * Input.GetAxis ("HorizontalR");
+		lift = liftSensitivity * Input.GetAxis ("VerticalR");
+
+		if (GlobalVars.invertYAxis)
+			turn *= -1.0f;
+		/*
+		if (GlobalVars.invertXAxis)
+			lift *= -1.0f;
+		*/
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		getInputs ();
+		rotateSide (turn);
+		zoom (lift);
+		adjustHeight ();
+		Vector3 collisionPoint = detectWall();
+		if (collisionPoint != Vector3.zero) 
+		{
+		
+			transform.position = collisionPoint;
+		}
+
+
+		transform.LookAt (target.transform.position);
+	}
+}
